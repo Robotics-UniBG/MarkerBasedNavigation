66e0d0077eccb3f51b4f8b81ed36797787ef8c16
lucaghera <lucaghera8@gmail.com>
2012-06-28 15:16:56 +0200

    Created package mbn_marker_locator

---
+++ mbn_marker_locator/include/mbn_marker_locator/MarkerLocatorNode.hpp
@@ -0,0 +1,141 @@
+/********************************************************************************
+ *
+ * MarkerLocatorNode
+ *
+ * Copyright (c) 2012
+ * All rights reserved.
+ *
+ * Luca Gherardi and Andrea Luzzana
+ * University of Bergamo
+ * Dept. of Information Technology and Mathematics
+ *
+ * -------------------------------------------------------------------------------
+ *
+ * File: MarkerLocatorComputation.hpp
+ * Created: Jan 20, 2012
+ *
+ * Author: <A HREF="mailto:luca.gherardi@unibg.it">Luca Gherardi</A>
+ * Author: <A HREF="mailto:andrea.luzzana@unibg.it">Andrea Luzzana</A>
+ *
+ * Supervised by: <A HREF="mailto:brugali@unibg.it">Davide Brugali</A>
+ *
+ * -------------------------------------------------------------------------------
+ *
+ * This sofware is published under a dual-license: GNU Lesser General Public
+ * License LGPL 2.1 and BSD license. The dual-license implies that users of this
+ * code may choose which terms they prefer.
+ *
+ * -------------------------------------------------------------------------------
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  - Neither the name of the University of Bergamo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version or the BSD license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License LGPL and the BSD license for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL and BSD license along with this program.
+ *
+ *******************************************************************************/
+#ifndef MARKER_LOCATOR_NODE_HPP
+#define MARKER_LOCATOR_NODE_HPP
+
+#include "ros/ros.h"
+#include "opencv2/opencv.hpp"
+#include <cv_bridge/cv_bridge.h>
+#include "sensor_msgs/Image.h"
+#include "sensor_msgs/image_encodings.h"
+#include "image_transport/image_transport.h"
+#include <opencv2/highgui/highgui.hpp>
+
+#include "mbn_common/MarkerLocatorComputation.hpp"
+#include "mbn_common/Marker.hpp"
+#include "mbn_common/Utility.hpp"
+
+using namespace mbn_common;
+//PROPERTIES
+// Camera setup and calibration
+string cameraDistorsionParamPath;
+int cameraWidth;
+int cameraHeight;
+
+// Marker detection
+int	blackWhiteThreshold;
+vector<int> thresholdToUse;
+double min_confidence;
+bool enableIDfilter;
+bool enableDetectOnlyTheBest;
+bool useBCH;
+geometry_msgs::Pose fixedCameratransform;
+double markersBaseWidth; //it's the default distance used for all markers
+vector<int> markersVectorIDtoFind;//the list of BHC ID that we want to use
+vector<double> markersDimensions;//the list of markers dimensions
+map<int, double> mapID2Dimension;
+
+//Objects
+/**
+ * MarkerLocatorComputation instance
+ */
+mbn_common::MarkerLocatorComputation *markerLocator;
+
+/**
+ * Utility class instance
+ */
+Utility utility;
+
+//Constants
+/**
+ * mm to meters conversion.
+ */
+static const double MM_TO_METERS = 0.001;
+
+//Publishers
+/**
+ * Marker Pose publisher.
+ */
+ros::Publisher markersPosesPublisher;
+
+/**
+ * Marker IDs publisher.
+ */
+ros::Publisher markersIDsPublisher;
+
+//Prototypes
+/**
+ * Configure the marker locator.
+ */
+bool configureMarkerLocator();
+
+/**
+ * Load data from file.
+ */
+bool loadConstants();
+
+/**
+ * Marker locator computationn step
+ * \param[in] openCvImage image to be elaborated.
+ */
+void MarkerLocatorStep(sensor_msgs::ImageConstPtr openCvImage);
+
+/**
+ * Callback function
+ * \param[in] imageMsg image to be elaborated.
+ */
+void imageCallback(const sensor_msgs::ImageConstPtr& imageMsg);
+#endif
---
+++ mbn_marker_locator/src/MarkerLocatorNode.cpp
@@ -0,0 +1,220 @@
+/********************************************************************************
+ *
+ * MarkerLocatorNode
+ *
+ * Copyright (c) 2012
+ * All rights reserved.
+ *
+ * Luca Gherardi and Andrea Luzzana
+ * University of Bergamo
+ * Dept. of Information Technology and Mathematics
+ *
+ * -------------------------------------------------------------------------------
+ *
+ * File: MarkerLocatorComputation.hpp
+ * Created: Jan 20, 2012
+ *
+ * Author: <A HREF="mailto:luca.gherardi@unibg.it">Luca Gherardi</A>
+ * Author: <A HREF="mailto:andrea.luzzana@unibg.it">Andrea Luzzana</A>
+ *
+ * Supervised by: <A HREF="mailto:brugali@unibg.it">Davide Brugali</A>
+ *
+ * -------------------------------------------------------------------------------
+ *
+ * This sofware is published under a dual-license: GNU Lesser General Public
+ * License LGPL 2.1 and BSD license. The dual-license implies that users of this
+ * code may choose which terms they prefer.
+ *
+ * -------------------------------------------------------------------------------
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  - Neither the name of the University of Bergamo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version or the BSD license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License LGPL and the BSD license for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL and BSD license along with this program.
+ *
+ *******************************************************************************/
+#include "mbn_marker_locator/MarkerLocatorNode.hpp"
+#include "brics_rn_msgs/MarkersPoses.h"
+#include "brics_rn_msgs/MarkerPose.h"
+#include "brics_rn_msgs/MarkersIDs.h"
+#include <yaml-cpp/yaml.h>
+#include <fstream>
+
+using namespace cv_bridge;
+using namespace sensor_msgs::image_encodings;
+using namespace std;
+using namespace mbn_common;
+
+//MAIN
+int main(int argc, char **argv) {
+
+	//Setup ros node and topics
+	ros::init(argc, argv, "rgb_marker_locator");
+	ros::NodeHandle nodeHandle;
+	image_transport::ImageTransport it(nodeHandle);
+	image_transport::Subscriber imageSubscriber = it.subscribe("image_topic", 1, imageCallback);
+	markersPosesPublisher = nodeHandle.advertise<brics_rn_msgs::MarkersPoses>("markers_poses_topic", 1000);
+	markersIDsPublisher = nodeHandle.advertise<brics_rn_msgs::MarkersIDs>("markers_ids_topic", 1000);
+
+	//Setup marker locator
+	loadConstants();
+	configureMarkerLocator();
+
+	//Go spinning
+	ros::spin();
+
+	return 0;
+}
+
+void imageCallback(const sensor_msgs::ImageConstPtr& imageMsg) {
+		MarkerLocatorStep(imageMsg);
+}
+
+bool configureMarkerLocator(){
+	markerLocator=new MarkerLocatorComputation("MarkerLocator");
+	if(!markerLocator->setCameraDistorsionParamPath(cameraDistorsionParamPath)){
+		ROS_ERROR("Error setting camera distorsion parameters");
+		return false;
+	}
+	if(!markerLocator->setCameraWidth(cameraWidth)){
+		ROS_ERROR("Error setting camera width");
+		return false;
+	}
+	if(!markerLocator->setCameraHeight(cameraHeight)){
+		ROS_ERROR("Error setting camera height");
+		return false;
+	}
+	if(!markerLocator->setMinConfidence(min_confidence)){//the minimal confidence used as edge to recognize the markers
+		ROS_ERROR("Error setting minimum confidence");
+		return false;
+	}
+	if(!markerLocator->setBlackWhiteThreshold(blackWhiteThreshold)){
+		ROS_ERROR("Error setting black/white threshold");
+		return false;
+	}
+	markerLocator->setEnableIDfilter(enableIDfilter);//if true, only the markers defined in markersVectorIDtoFind will be searched, you can enable this only if enableDetectOnlyTheBest is disabled
+	markerLocator->setEnableDetectOnlyTheBest(enableDetectOnlyTheBest);//if true, marker locator will look for the marker with the best confidence in the image
+	markerLocator->setUseBCH(useBCH);
+
+	markerLocator->setMarkersBaseWidth(markersBaseWidth);//it's the default distance used for all markers
+
+	markerLocator->markersVectorIDtoFind=markersVectorIDtoFind;//the list of BHC ID that we want to use
+	markerLocator->markersDimensions=markersDimensions;//the list of markers dimensions
+	markerLocator->setCameraPose(fixedCameratransform);
+
+	return markerLocator->configureMarkerLocatorComputation();
+
+
+}
+
+bool loadConstants(){
+
+	string pathFile = "configuration_marker_locator.yaml";
+	std::ifstream pathDescritptorFile(pathFile.c_str());
+	if (pathDescritptorFile.fail()) {
+		ROS_ERROR("Configuration file cannot be opened %s", pathFile.c_str());
+		return false;
+	}
+	YAML::Parser parser(pathDescritptorFile);
+	YAML::Node doc;
+	parser.GetNextDocument(doc);
+
+	try {
+		doc["cameraDistorsionParamPath"] >> cameraDistorsionParamPath;
+		doc["cameraWidth"] >> cameraWidth;
+		doc["cameraHeight"] >> cameraHeight;
+		doc["blackWhiteThreshold"] >> blackWhiteThreshold;
+		doc["min_confidence"] >> min_confidence;
+		doc["enableIDfilter"] >> enableIDfilter;
+		doc["enableDetectOnlyTheBest"] >> enableDetectOnlyTheBest;
+		doc["useBCH"] >> useBCH;
+
+		doc["fixedCameratransform_orientation_x"] >> fixedCameratransform.orientation.x;
+		doc["fixedCameratransform_orientation_y"] >> fixedCameratransform.orientation.y;
+		doc["fixedCameratransform_orientation_z"] >> fixedCameratransform.orientation.z;
+		doc["fixedCameratransform_orientation_w"] >> fixedCameratransform.orientation.w;
+		doc["fixedCameratransform_position_x"] >> fixedCameratransform.position.x;
+		doc["fixedCameratransform_position_y"] >> fixedCameratransform.position.y;
+		doc["fixedCameratransform_position_z"] >> fixedCameratransform.position.z;
+		doc["markersBaseWidth"] >>  markersBaseWidth;
+
+		const YAML::Node& markersIDs = doc["markersVectorIDtoFind"];
+		for(unsigned i=0;i<markersIDs.size();i++) {
+			int id;
+			markersIDs[i] >> id;
+			markersVectorIDtoFind.push_back(id);
+		}
+
+		const YAML::Node& markersSize = doc["markersDimensions"];
+		for(unsigned i=0;i<markersSize.size();i++) {
+			int size;
+			markersSize[i] >> size;
+			markersDimensions.push_back(size);
+		}
+
+	}catch (YAML::InvalidScalar&) {
+		ROS_ERROR("Something went wrong during the parsing of configuration file");
+		return false;
+	}
+
+
+	return true;
+}
+
+void MarkerLocatorStep(sensor_msgs::ImageConstPtr imageInput){
+
+	cv_bridge::CvImageConstPtr openCvImage;
+	try{
+		// convert ROS image to openCv image,
+		// sharing it when possible
+		openCvImage = cv_bridge::toCvShare(imageInput,"bgr8");
+	}catch (cv_bridge::Exception & e){
+		ROS_ERROR("Could not convert from %s to 'bgr8'.", imageInput->encoding.c_str());
+	}
+
+	brics_rn_msgs::MarkersPoses detectedMarkersPoses;
+	brics_rn_msgs::MarkersIDs detectedMarkersIDs;
+
+	vector<int> vectorMarkersIDsFound;
+	vector<tf::Pose> vectorMarkersPosesFound;
+	markerLocator->findMarkers(openCvImage.get()->image.data,&vectorMarkersIDsFound,&vectorMarkersPosesFound);
+
+	for(int x=0; x < vectorMarkersIDsFound.size(); x++){
+		int currentMarkerID = vectorMarkersIDsFound.at(x);
+		brics_rn_msgs::MarkerPose markerPose=brics_rn_msgs::MarkerPose();
+		markerPose.marker_id=currentMarkerID;
+		markerPose.header.stamp= imageInput->header.stamp;
+		//log info useful for debugging
+		ROS_INFO("Marker found ID: %i ", markerPose.marker_id);
+		ROS_INFO("%s",utility.getPositionAndOrientation(vectorMarkersPosesFound.at(x)).c_str());
+
+		tf::poseTFToMsg(vectorMarkersPosesFound.at(x), markerPose.poseWRTRobotFrame);
+
+		detectedMarkersPoses.markersPoses.push_back(markerPose);
+		detectedMarkersIDs.markersIDs.push_back(markerPose.marker_id);
+	}
+	markersPosesPublisher.publish(detectedMarkersPoses);
+	markersIDsPublisher.publish(detectedMarkersIDs);
+
+}
+
