fa98833defa2d701c8ddb128436ba5e6679c6902
lucaghera <lucaghera8@gmail.com>
2012-06-28 15:24:23 +0200

    Created package mbn_visualizer

---
+++ mbn_visualizer/src/VisualizerNode.cpp
@@ -0,0 +1,84 @@
+/********************************************************************************
+ *
+ * VisualizerNode
+ *
+ * Copyright (c) 2012
+ * All rights reserved.
+ *
+ * Luca Gherardi and Andrea Luzzana
+ * University of Bergamo
+ * Dept. of Information Technology and Mathematics
+ *
+ * -------------------------------------------------------------------------------
+ *
+ * File: MarkerLocatorComputation.hpp
+ * Created: Jan 20, 2012
+ *
+ * Author: <A HREF="mailto:luca.gherardi@unibg.it">Luca Gherardi</A>
+ * Author: <A HREF="mailto:andrea.luzzana@unibg.it">Andrea Luzzana</A>
+ *
+ * Supervised by: <A HREF="mailto:brugali@unibg.it">Davide Brugali</A>
+ *
+ * -------------------------------------------------------------------------------
+ *
+ * This sofware is published under a dual-license: GNU Lesser General Public
+ * License LGPL 2.1 and BSD license. The dual-license implies that users of this
+ * code may choose which terms they prefer.
+ *
+ * -------------------------------------------------------------------------------
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  - Neither the name of the University of Bergamo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version or the BSD license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License LGPL and the BSD license for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL and BSD license along with this program.
+ *
+ *******************************************************************************/
+#include "mbn_visualizer/VisualizerNode.hpp"
+using namespace cv_bridge;
+using namespace sensor_msgs::image_encodings;
+
+
+
+int main(int argc, char **argv) {
+	//Setup ros node and topics
+	ros::init(argc, argv, "mbn_visualizer");
+	ros::NodeHandle nodeHandle;
+
+	image_transport::ImageTransport it(nodeHandle);
+	image_transport::Subscriber sub = it.subscribe("image_topic", 1, imageCallback);
+
+	//Go spinning
+	ros::spin();
+
+	return 0;
+}
+
+void imageCallback(const sensor_msgs::ImageConstPtr& imageMsg) {
+	//Convert image
+	CvImageConstPtr openCvImage = toCvShare(imageMsg,"bgr8");
+	IplImage ipl(openCvImage.get()->image);
+
+	//Show image
+	cv::imshow("mbn_visualizer", openCvImage.get()->image);
+	cv::waitKey(3);
+}
